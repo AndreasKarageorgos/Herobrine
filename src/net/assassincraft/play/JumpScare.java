fe5f8ab621a4ed3333c21af009069cccd3fd0bdd
AndreasKarageorgos <andreas_karageorgos@protonmail.com>
2021-06-27 18:28:05 +0300

    commit
    Signed-off-by: AndreasKarageorgos <andreas_karageorgos@protonmail.com>

--- src/net/assassincraft/play/JumpScare.java
+++ src/net/assassincraft/play/JumpScare.java
@@ -20,21 +20,10 @@ public class JumpScare {
 	}
 	
 	public void scare(Player player) {
-		boolean wasWalking;
-		float speed;
-		
 		Herobrine brine = new Herobrine(player);
 		brine.spawn();
 		if(player.isSprinting()){player.setSprinting(false);};
-		if(player.isFlying()) {
-			speed = player.getFlySpeed();
-			player.setFlySpeed(0);
-			wasWalking = false;
-		}else {
-			speed = player.getWalkSpeed();
-			player.setWalkSpeed(0);
-			wasWalking = true;
-		}
+		player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 65, 20));
 		player.playSound(player.getLocation(),Sound.ENTITY_ZOMBIE_BREAK_WOODEN_DOOR, 5, 0.5f);
 		player.playSound(player.getLocation(),Sound.ENTITY_ZOMBIE_BREAK_WOODEN_DOOR, 7, 0.8f);
 		player.playSound(player.getLocation(),Sound.ENTITY_ZOMBIE_BREAK_WOODEN_DOOR, 10, 1);
@@ -46,15 +35,9 @@ public class JumpScare {
 			int count = 0;
 			public void run() {
 				if(count % 10 ==0) {
-					player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 2, 3));
 					player.playSound(player.getLocation(),Sound.ENTITY_ENDER_DRAGON_SHOOT, 10, 0.1f);
 				}
 				if(count>=60) {
-					if(wasWalking) {
-						player.setWalkSpeed(speed);
-					}else {
-						player.setFlySpeed(speed);
-					}
 					player.playSound(player.getLocation(),Sound.ENTITY_WITCH_CELEBRATE, 5, 0.4f);
 					player.playSound(player.getLocation(),Sound.ENTITY_WITCH_CELEBRATE, 10, 0.2f);
 					player.sendMessage(ChatColor.GOLD + "HEROBRINE: " + ChatColor.RED +ChatColor.BOLD + "HAHAHA!");
--- src/net/assassincraft/play/NPC/Herobrine.java
+++ src/net/assassincraft/play/NPC/Herobrine.java
@@ -32,7 +32,7 @@ public class Herobrine {
 	
 	public void spawn() {
 		Location loc = player.getLocation();
-		double distance = 2.5;
+		double distance = 2.0;
 		if(player.isSprinting()) {
 			distance = 3.5;
 		}
@@ -46,12 +46,12 @@ public class Herobrine {
 		}else if(player.getFacing() == BlockFace.EAST){
 			loc.setX(loc.getX() + distance);
 		}
+		
 		loc.setDirection(loc.getDirection().multiply(-1));
 		getNpc().setLocation(loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch());
 	}
 	
 	private GameProfile getSkin() {
-		// Skin #2022841354 generated on May 29, 2021 7:10:43 PM via MineSkin.org - https://minesk.in/2022841354
 		GameProfile skin2022841354 = new GameProfile(UUID.fromString("4f6fbd1e-1c77-418c-9e78-7c94e37ed710"),"");
 		skin2022841354.getProperties().put("textures", new Property("textures", "ewogICJ0aW1lc3RhbXAiIDogMTYyMjMwNDY0MjU3MSwKICAicHJvZmlsZUlkIiA6ICI0NWY3YTJlNjE3ODE0YjJjODAwODM5MmRmN2IzNWY0ZiIsCiAgInByb2ZpbGVOYW1lIiA6ICJfSnVzdERvSXQiLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMTUyM2UxZjA2ZDhlMTM5OWMwNjhhYTU3NjhmMDUzMDI1N2FhYjIxYTM0ZTk1NTY5MzNlNjM0NTUxODY0MjVhNiIKICAgIH0KICB9Cn0=", "PAL4UKYIgCe3RwmxfT6p05DDqc0kW2HTvlNNRuP3npXyuL68Hifjtiuu6OiJ/IQ/YGsjKf3Ni219teFFBJqEUJNEskC7CWnJxHWRq/WkSNDJXVKvBW6JjOYN607HSlBhifn4ZAxasol0bqVRURVG+0BwDUHXIGy4LRSu10/SLhsAmvwKhwYJiQ1xgnerqfzeWmPQSDZOo30Fxo6jNRnJ7v0VBGR8awImOfVsKE3SuEeys47U5AqXrHIp88s36aGOsIa0Bs6NHutW/L6Wbg9BHGUs93NGnjP9jxXizhGITF45KBXNl7gXPPjMnK7/CGOTuxuABc+MVAptB4cHCksnfaSMexryluQuwzWzOJTX1NCMGlYFREDq4c3GgVWJ7Yo2xWOwcy+XZxJXNchQ5/vzA3dv+XdU4zcD93OOhuv9PDoUTlmQgLFQLFbxM6Bq6cSQg3Wybi7wO6jQzBIVpZaswrOo/wgHuXbDkA9rv5uONjQQbKHJQjVjgIrbbUpX3+Kh4MkgBEUztxNvbgbt5RK0knwtYhscKDyrP5AAwp9CX6JnmbKXIRVWtx6b3JNPYC9I6FtBCoGxo6OfHl3f/okncszIh+40Sif3IdMrW+H5g3MxMkn5eHP4nOHRL24izlLUM52gS/TUgpG0dJrG6XMOR3w6KqTxcT/4vyxwlmJ+iDg="));
 		return skin2022841354;
